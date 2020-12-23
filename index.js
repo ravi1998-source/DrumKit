5a5ac0eb6775c3f5f4cc8c63ec2d2baf69f04832
Raviteja <66660789+ravi1998-source@users.noreply.github.com>
2020-12-23 16:07:13 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,90 @@
+
+
+//Detecting Button Press
+
+var numOfDrumButtons = document.querySelectorAll(".drum").length;
+	
+
+
+ for(var i=0; i<numOfDrumButtons; i++){
+
+	document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+      
+	  
+
+	 var buttonInnerHTML = this.innerHTML;
+
+	 makeSound(buttonInnerHTML);
+      
+     buttonAnimation(buttonInnerHTML);
+
+});
+  
+}
+
+//Detecting KeyBoard Press
+
+
+document.addEventListener("keydown",function(event){
+
+  makeSound(event.key);
+  buttonAnimation(event.key);
+
+});
+
+function makeSound(key)  {
+
+
+	switch(key){
+
+		case'w':
+	 	var tom_1 = new Audio('sounds/tom-1.mp3');
+	 	tom_1.play();
+	 	break;
+
+	 	case'a':
+	 	var tom_2 = new Audio('sounds/tom-2.mp3');
+	 	tom_2.play();
+	 	break;
+
+	 	case's':
+	 	var tom_3 = new Audio('sounds/tom-3.mp3');
+	 	tom_3.play();
+	 	break;
+
+        case'd':
+	 	var tom_4 = new Audio('sounds/tom-4.mp3');
+	 	tom_4.play();
+        
+        case'j':
+	 	var crash = new Audio('sounds/crash.mp3');
+	 	crash.play(); 
+        
+        case'k':
+	 	var kick = new Audio('sounds/kick-bass.mp3');
+	 	kick.play();
+	 	break;
+
+	 	case'l':
+	 	var snare = new Audio('sounds/snare.mp3');
+	 	snare.play();
+	 	break;
+
+	 	default:
+	 	console.log(key);
+	 	break;
+
+	}
+}
+
+function buttonAnimation(currentKey) {
+
+	var activeButton = document.querySelector("." + currentKey);
+	activeButton.classList.add("pressed");
+
+	setTimeout(function(){
+		activeButton.classList.remove("pressed")
+	}, 100);
+}
+
+
